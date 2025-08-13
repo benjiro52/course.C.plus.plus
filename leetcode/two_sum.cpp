4a1286473548f467eb55d032580ebb33a3882538
Ivan <i.stahanov2000@gmail.com>
2025-08-13 18:00:50 +0200

    12.08.25

---
+++ leetcode/two_sum.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, target, sum = 0, point = 2, zero = 0;
+    //cin >> n;
+
+    vector<int> nums(5);
+    for (int i = 0; i < nums.size(); i++) {
+        cin >> nums[i];
+    }
+    cin >> target;
+
+    for (int i = zero; i < point; i++) {
+        sum += nums[i];
+        if  (sum < target || sum > target) {
+            zero++;
+            point++;
+            sum = 0;
+            continue;
+        }
+        if (sum == target) {
+            cout << nums[i - 1] << " " << nums[i];
+            break;
+        }
+    }
+}
