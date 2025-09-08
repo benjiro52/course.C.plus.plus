efe6b8b7423201a5e16b98b3a4beb1cc66a64848
Ivan <i.stahanov2000@gmail.com>
2025-09-08 18:04:58 +0200

    08.09.25

--- sumi.cpp
+++ sumi.cpp
@@ -2,5 +2,30 @@
 using namespace std;
 
 int main() {
-    
+    int n, m;
+    cin >> n >> m;
+
+    int a[100][100];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    vector<int> sum;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (i + 1 < n) {
+                sum.push_back(a[i][j] + a[i + 1][j]);
+            }
+            if (j + 1 < m) {
+                sum.push_back(a[i][j] + a[i][j + 1]);
+            }
+        }
+    }
+
+    sort(sum.begin(), sum.end());
+    cout << sum.front() << " " << sum.back();
+
+    return 0;
 }
