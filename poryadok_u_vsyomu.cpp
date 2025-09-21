d421c494f97849baf0ef80b0aeb9990725d88187
Ivan <i.stahanov2000@gmail.com>
2025-09-21 18:02:06 +0200

    21.09.25

--- poryadok_u_vsyomu.cpp
+++ poryadok_u_vsyomu.cpp
@@ -2,5 +2,27 @@
 using namespace std;
 
 int main() {
-        
+    int n;
+    cin >> n;
+    string a;
+    cin >> a;
+    for (int i = 1; i < n; i++) {
+        string b;
+        cin >> b;
+        if (a.size() > b.size()) {
+            string ap = a.substr(0, b.size());
+            if (ap == b) {
+                b = a;
+            } else if (ap > b) {
+                b += string(a.size() + 1 - b.size(), '0');
+            } else {
+                b += string(a.size() - b.size(), '0');
+            }
+        }
+        a = b;
+    }
+    cout << a;  
+
+    return 0;
 }
+
--- test_2.cpp
+++ test_2.cpp
@@ -4,34 +4,25 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-
-    string shields;
-    while ((int)shields.size() < n) {
-        char c;
-        if (!(cin >> c)) {
-            break;
-        } 
-        if (c == '(' || c == ')') {
-            shields.push_back(c);
-        }
-    }
-
-    int left = 1;
+    string a;
+    cin >> a;
     for (int i = 1; i < n; i++) {
-        if (shields[i] == shields[0]) left++;
-        else break;
+        string b;
+        cin >> b;
+        if (a.size() > b.size()) {
+            string ap = a.substr(0, b.size());
+            if (ap == b) {
+                b = a;
+            } else if (ap > b) {
+                b += string(a.size() + 1 - b.size(), '0');
+            } else {
+                b += string(a.size() - b.size(), '0');
+            }
+        }
+        a = b;
     }
+    cout << a;  
 
-    int right = 1;
-    for (int i = n - 2; i >= 0; i--) {
-        if (shields[i] == shields[n - 1]) right++;
-        else break;
-    }
-
-    if (shields[0] != shields[n - 1]) {
-        cout << n;
-    } else {
-        cout << (n - min(left, right));
-    }
     return 0;
 }
+
