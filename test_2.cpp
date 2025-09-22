fd6195429309378eb5b090408bc86f595bd55477
Ivan <i.stahanov2000@gmail.com>
2025-09-22 09:07:35 +0200

    22.09.25(2)

--- test_2.cpp
+++ test_2.cpp
@@ -2,27 +2,24 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
-    string a;
-    cin >> a;
-    for (int i = 1; i < n; i++) {
-        string b;
-        cin >> b;
-        if (a.size() > b.size()) {
-            string ap = a.substr(0, b.size());
-            if (ap == b) {
-                b = a;
-            } else if (ap > b) {
-                b += string(a.size() + 1 - b.size(), '0');
-            } else {
-                b += string(a.size() - b.size(), '0');
-            }
-        }
-        a = b;
-    }
-    cout << a;  
+    long long n, m, k;
+    cin >> n >> m >> k;
+
+    long long right = n - 1;
+    long long up = m - 1;
+
+    long long diagonal = min(right, up);
+    long long steps_diagonal = (diagonal + k - 1) / k;
+
+    long long remain_right = right - diagonal;
+    long long remain_up = up - diagonal;
+
+    long long steps_right = (remain_right + k - 1) / k;
+    long long steps_up = (remain_up + k - 1) / k;
+
+    long long ans = steps_diagonal + steps_right + steps_up;
+
+    cout << ans;
 
     return 0;
 }
-
