69311a23d4914526a12a32f54b3909b0d1fe70bb
Ivan <i.stahanov2000@gmail.com>
2025-10-22 09:35:12 +0200

    22.10.25

---
+++ projects_by_myself/class.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class human {
+public:
+    int age;
+    int weight;
+    string name;
+
+};
+
+class point {
+public:
+    int x;
+    int y;
+    int z;
+};
+
+int main() {
+    point a;
+    a.x = 1;
+    a.y = 2;
+    a.z = 3;
+}
