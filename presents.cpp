2716791b964ba0aa1ff71ca4fda95efcb12cabe9
Ivan <i.stahanov2000@gmail.com>
2025-10-27 17:50:29 +0100

    27.10.25

--- presents.cpp
+++ presents.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
-    
+    long long n, a, b, ant = 0;
+    cin >> n >> a >> b;
+
+    while (true) {
+        long long max_3 = min(b, n / 3);
+        long long need_1 = n - max_3 * 3;
+        if (need_1 <= a && max_3 >= 0) {
+            ant++;
+            a -= need_1;
+            b -= max_3;
+        } else if (a >= n) {
+            ant++;
+            a -= n;
+        } else {
+            break;
+        }
+    }
+    cout << ant;
+
+    return 0;
 }
--- test_2.cpp
+++ test_2.cpp
@@ -2,8 +2,33 @@
 using namespace std;
 
 int main() {
-    long long N, M;
-    cin >> N >> M;
-    cout << N * M - N - M + 1;
-    return 0;
+    long long N, A, B;
+    cin >> N >> A >> B;
+
+    long long ans = 0;
+
+    while (true) {
+        bool ok = false;
+        if (N >= 3 && B > 0) {
+            long long need3 = min(B, N / 3);
+            long long rest = N - need3 * 3;
+
+            if (rest <= A) {
+                A -= rest;
+                B -= need3;
+                ans++;
+                ok = true;
+            }
+        }
+
+        if (!ok && A >= N) {
+            A -= N;
+            ans++;
+            ok = true;
+        }
+
+        if (!ok) break;
+    }
+
+    cout << ans;
 }
