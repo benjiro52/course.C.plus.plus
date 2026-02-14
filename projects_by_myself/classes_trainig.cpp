cc418cfb1ed11b536c2bc10e42b64439ac304186
Ivan <i.stahanov2000@gmail.com>
2026-02-14 15:06:30 +0100

    trainig

--- projects_by_myself/classes_trainig.cpp
+++ projects_by_myself/classes_trainig.cpp
@@ -11,13 +11,26 @@ public:
         cout << "Color: " << color << endl << "Speed: " << speed;
     }
 };
+
+class mustang {
+public: 
+    int speed;
+    int price;
+
+    void showInfoMustang() {
+        cout << speed << " " << price;
+    }
+};
     
 int main() {
     car myCar;
-
     myCar.color = "black";
     myCar.speed = 200;
-    
     myCar.showInfo();
+    cout << endl;
 
+    mustang mus;
+    mus.speed = 300;
+    mus.price = 80000;
+    mus.showInfoMustang();
 }
