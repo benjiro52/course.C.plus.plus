d15611291ed6f5189511c1af3f1d888d7279efa2
Ivan <i.stahanov2000@gmail.com>
2026-02-21 14:19:27 +0100

    test_calude

--- test_2.cpp
+++ test_2.cpp
@@ -1,43 +1,11 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
-    int w, h, k;
-    cin >> w >> h >> k;
-    int ant = 0;
-
-    for (int a = 1; a <= w; ++a) {
-        if (w % a != 0) {
-            continue;
-        }
-        for (int b = 1; b <= h; ++b) {
-            if (h % b != 0) {
-                continue;
-            }
-            int cnt = (w / a) * (h / b);
-            if (cnt == k) {
-                ant++;
-            }
-        }
-    }
-    if (w != h) {
-        for (int a = 1; a <= h; ++a) {
-            if (h % a != 0){
-                continue;
-            } 
-            for (int b = 1; b <= w; ++b) {
-                if (w % b != 0) {
-                    continue;
-                }
-                if (a == b) {
-                    continue;
-                } 
-                int cnt = (h / a) * (w / b);
-                if (cnt == k) {
-                    ant++;
-                }
-            }
-        }
-    }
-    cout << ant;
+    cout << "Hello, World!" << endl;
+    
+    int number = 42;
+    cout << "The answer is: " << number << endl;
+    
+    return 0;
 }
