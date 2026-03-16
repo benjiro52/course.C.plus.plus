a62e699c88aa6aee4afebc5bde08c7382cade384
Ivan <i.stahanov2000@gmail.com>
2026-03-16 18:07:58 +0100

    16.03.26

---
+++ parol_vid_safe.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int n = s.size();
+
+    vector<int> bad;
+    for (int i = 0; i < n / 2; i++) {
+        if (s[i] != s[n - 1 - i]) {
+            bad.push_back(i);
+        }
+    }
+
+    if (bad.size() == 0) {
+        cout << "YES";
+        return 0;
+    }
+
+    if (bad.size() == 1 && n % 2 == 1) {
+        cout << "YES";
+        return 0;
+    }
+
+    if (bad.size() == 2) {
+        int i = bad[0], j = bad[1];
+        if (s[i] == s[n - 1 - j] && s[j] == s[n - 1 - i]) {
+            cout << "YES";
+            return 0;
+        }
+    }
+
+    cout << "NO";
+    return 0;
+}
