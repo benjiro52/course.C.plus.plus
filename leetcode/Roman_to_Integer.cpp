fd5474279b88dfd9f11321f46dc60ae4d5350bdc
Ivan <i.stahanov2000@gmail.com>
2026-06-11 16:51:15 +0200

    roman_to_integer

---
+++ leetcode/Roman_to_Integer.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "MCMXCIV";
+    int ans = 0;
+    unordered_map<char, int> mp = {
+        {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+        {'C', 100}, {'D', 500}, {'M', 1000}
+    };
+    
+
+    for (int i = s.size() - 1; i >= 0; i--) {
+        // cout << mp[s[i]] << " ";
+        // ans += mp[s[i]];
+
+        if (mp[s[i]] < mp[s[i - 1]]) {
+            ans -= mp[s[i]];
+        } else {
+            ans += mp[s[i]];
+        }
+        cout << ans << " ";
+    }
+    cout << endl << ans;
+}
