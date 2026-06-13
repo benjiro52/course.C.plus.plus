b7b388fd69ad6361e56a05c3df4a2c2430afffd8
Ivan <i.stahanov2000@gmail.com>
2026-06-13 09:36:11 +0200

    string_training

---
+++ leetcode/longest_common_prefix.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<string> strs = {"flower", "flow", "flight"};
+    string prefix = strs[0];
+    
+    for (int i = 1; i < strs.size(); i++) {
+        while (strs[i].find(prefix) != 0) {
+            prefix = prefix.substr(0, prefix.size() - 1);
+        }
+    }
+
+    cout << prefix;
+}
