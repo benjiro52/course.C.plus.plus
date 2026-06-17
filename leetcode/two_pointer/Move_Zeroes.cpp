dd69c81dcff8c3f04ae70815c521a480c9ed5c3b
Ivan <i.stahanov2000@gmail.com>
2026-06-17 14:34:45 +0200

    two_pointer_trainig

---
+++ leetcode/two_pointer/Move_Zeroes.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> nums = {0,1,0,3,12};
+    int slow = 0;
+    // fast slow to pointer
+    for (int fast = 1; fast < nums.size(); fast++) {
+        if (nums[fast] != 0) {
+            swap(nums[slow], nums[fast]);
+            slow++;
+        }
+    }
+
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
+}
