9066fed9b8187651cdac444fe7f69a85a5077bbe
Ivan <i.stahanov2000@gmail.com>
2026-06-17 14:43:38 +0200

    17.06.26_update

--- leetcode/two_pointer/Move_Zeroes.cpp
+++ leetcode/two_pointer/Move_Zeroes.cpp
@@ -5,7 +5,7 @@ int main() {
     vector<int> nums = {0,1,0,3,12};
     int slow = 0;
     // fast slow to pointer
-    for (int fast = 1; fast < nums.size(); fast++) {
+    for (int fast = 0; fast < nums.size(); fast++) {
         if (nums[fast] != 0) {
             swap(nums[slow], nums[fast]);
             slow++;
