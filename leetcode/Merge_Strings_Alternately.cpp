ee8b875854510ab507a02dc3b9a3b48a51534955
Ivan <i.stahanov2000@gmail.com>
2026-07-01 22:44:01 +0200

    day_29

---
+++ leetcode/Merge_Strings_Alternately.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string word1 = "abc", word2 = "pqr";
+
+    int w1 = 0;
+    int w2 = 0;
+
+    for (int i = 0; i < word1.size(); i++) {
+       cout << word1[w1];
+       w1++;
+
+       cout << word2[w2]; 
+       w2++;
+    }
+
+    return 0;
+}
