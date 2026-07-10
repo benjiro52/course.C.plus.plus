e58ed789b0476fab190a670e8d23dd3ef2541011
Ivan <i.stahanov2000@gmail.com>
2026-07-10 12:43:35 +0200

    cleaning

---
+++ projects/hangman.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector <string> words = {"Akashi", "Kise", "Midorima", "Murasakibara", "Kuroku", "Aomine"};
+    vector <string> symbols_words = {"_ _ _ _ _ _", "_ _ _ _", "_ _ _ _ _ _ _ _", "_ _ _ _ _ _", "_ _ _ _ _ _"};
+    vector <string> 
+
+    srand(time(0));
+    int index = rand() % 6; 
+    cout << "Your word is: " << symbols_words[index] << endl << endl;
+
+    cout << "Your letter: ";
+
+
+    
+}
