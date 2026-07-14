a6a3f63f8200f8a8f72bd0219f6fd8485394e753
Ivan <i.stahanov2000@gmail.com>
2026-07-14 19:52:05 +0200

    changes_day41

--- myself_lerning/classes/class_nasledovanie_3.cpp
+++ myself_lerning/classes/class_nasledovanie_3.cpp
@@ -10,7 +10,7 @@ public:
         brand = brand_;
     }
 
-    void accelarate(int amount) {
+    void accelerate(int amount) {
         speed += amount;
     }
     void printStatus() {
@@ -20,13 +20,34 @@ public:
 
 class Car : public Vehicle {
 private:
-    int numDoors = 4;
+    int numDoors;
 public:
-    Car (int doors) : Vehicle(brand) {
+    Car (string brand_, int doors) : Vehicle(brand_) {
         numDoors = doors;
     } 
+    void honk() {
+        cout << "HONK HONK" << endl;
+    }
+};
+
+class Bike : public Vehicle {
+private:
+    bool has_basket;
+public:
+    Bike (string brand_, bool has_basket_) : Vehicle (brand_) {
+        has_basket = has_basket_;
+    } 
+    void ring_bell() {
+        cout << "RING RING" << endl;
+    }
 };
 
 int main() {
+    Car cr("Mercedes", 4);
+    cr.accelerate(100);
+    cr.printStatus();
+    cr.honk();
 
+    Bike bk("Top", true);
+    bk.ring_bell();
 }
--- myself_lerning/ukazateli.cpp
+++ myself_lerning/ukazateli.cpp
@@ -4,11 +4,14 @@ using namespace std;
 int main() {
     int i = 0;
     cout << &i << endl;
+
     int* ptr = &i;
-    cout << ptr << endl;
+    cout << "ptr: " << ptr << endl;
 
     char c = 'a';
     char* ptr2 = &c;
+    cout << "ptr2: " << (void*)ptr2 << endl;
     *ptr2 = 'b';
+    cout << "ptr2.1: " << ptr2 << endl;
     cout << c << endl;
 }
