262962304e3eafc789182b3e53347b8a55e6a0bf
Ivan <i.stahanov2000@gmail.com>
2026-07-16 11:28:23 +0200

    day43_codewars_and_leetcode

---
+++ test_1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n = 942;
+    int ans = 0;
+    while (n > 0) {
+        ans += n % 10;
+        n / 10;
+    }
+    cout << ans;
+}
