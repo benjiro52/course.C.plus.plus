ca75a46dcbf15edb07d9a693d99f1ec92afd7a59
Ivan <i.stahanov2000@gmail.com>
2026-07-19 17:10:51 +0200

    newtest

--- test_1.cpp
+++ test_1.cpp
@@ -1,15 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-auto add(int n) {
-    return [n](int m) {
-        int ans = 0;
-        ans += n;
-        ans += m;
-        return ans;
-    };
-}
-
-int main() {
-    cout << add(1)(2); // выведет 3
+int main () {
+    string a = "hello";
 }
